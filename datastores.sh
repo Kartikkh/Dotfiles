4a76048ef811ad2347260e48d04f9c5b8b64d075
kartikkh <khandelwal.kartik4@gmail.com>
2018-04-27 17:11:03 +0530

    Datastore added

---
+++ datastores.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+
+
+# Install data stores
+brew install mysql
+brew install postgresql
+brew install mongo
+brew install redis
+brew install elasticsearch
+
+
+
+brew cleanup
